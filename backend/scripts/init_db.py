359a3a621ad82f41b3c0a418a44a46228d8ef225
jfeng3 <60714775+jfeng23@users.noreply.github.com>
2022-11-04 20:03:02 -0400

    zipcode arr

--- backend/scripts/init_db.py
+++ backend/scripts/init_db.py
@@ -12,7 +12,6 @@ with open('schema.sql') as f:
     conn.executescript(f.read())
 
 conn.commit()
-conn.close()
 
 print("Tables created successfully")
 
@@ -22,11 +21,28 @@ print("Tables created successfully")
 
 path = '..' + os.sep + 'data' + os.sep
 
+zipcodes = []
+
 for csv in os.listdir(path):
     if "MD_Covid" in csv:
+        if "2020" in csv: year = "2020"
+        elif "2021" in csv: year = "2021"
+        elif "2022" in csv: year = "2022"
+
         table = pd.read_csv(path + csv)
         for i, row in table.iterrows():
-            pass
+            # row.col is the format for accessing each column / attribute
+            zip = row.ZIP_CODE
+            zipcodes.append(zip)
+            for i in range(1, 12):
+                if i < 10: month = f"0{i}"
+                month_date = f"total{month}_1_{year}"
+                print(month_date)
+                try:
+                    date = row.month_date
+                except:
+                    pass
+            
         print(f"{csv} done")
 
 # when loading crime data, check if coor reverese geocoded is in zipcode table; if not, add to zipcode table
