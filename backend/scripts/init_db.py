0830df21c3b8160965ab1b639e9db6b0470c66f9
jfeng3 <60714775+jfeng23@users.noreply.github.com>
2022-11-04 21:18:47 -0400

    Update init_db.py

--- backend/scripts/init_db.py
+++ backend/scripts/init_db.py
@@ -12,18 +12,9 @@ conn = sqlite3.connect('baltimore_db.sqlite', check_same_thread=False)
 
 with open('schema.sql') as f:
     conn.executescript(f.read())
-
 conn.commit()
 
 print("Tables created successfully")
-
-# scan for all MD_Covid_...csvs, loop each csv, each row, insert to covid_cases table
-# load corresponding zipcodes and coordinates from geopy geocode to zipcode table
-# zipcode array for tracking
-
-path = '..' + os.sep + 'data' + os.sep
-
-zipcodes = []
 
 def get_zipcode_from_address(address):
     geolocator = Nominatim(user_agent="bccv")
@@ -31,8 +22,21 @@ def get_zipcode_from_address(address):
     zipcode = location.address.split(",")[-2].strip()
     return zipcode
 
-print("loading tables into sql...")
-for csv in os.listdir(path):
+
+# ----------------------load all csv files into db----------------------
+
+# scan for all MD_Covid_...csvs, loop each csv, each row, insert to covid_cases table
+# load corresponding zipcodes and coordinates from geopy geocode to zipcode table
+# zipcode array for tracking
+
+# when loading crime data, check if coor reverese geocoded is in zipcode table; if not, add to zipcode table
+# ^^^ to add any missing zipcodes missed from MD_Covid data
+
+PATH = '..' + os.sep + 'data' + os.sep
+zipcodes = []
+
+print("Loading tables into sql...")
+for csv in os.listdir(PATH):
 
     # load COVID total_cases into sql
     if "MD_Covid" in csv:
@@ -42,9 +46,9 @@ for csv in os.listdir(path):
 
         geolocator = Nominatim(user_agent="bccv")
 
-        table = pd.read_csv(path + csv)
+        table = pd.read_csv(PATH + csv)
         for i, row in table.iterrows():
-            # row.col OR row[col] 
+            # row.col OR row[col]
             zip = int(row.ZIP_CODE)
             if 21201 <= zip and zip <= 21298:
                 # create row in zipcode table if it doesnt exist
@@ -58,24 +62,25 @@ for csv in os.listdir(path):
                     cur.execute('INSERT INTO zipcode (zipcode, lat, long) VALUES(?, ?, ?)', params)
 
                 # grab months 01 to 12 for each zipcode
+
                 for i in range(1, 13):
                     if i < 10: m = f"0{i}"
                     else: m = i
-
-                    date = f"total{m}_01_{year}"
-                    case_count = row[date]
-
-                    if not math.isnan(case_count):
-                        params = (date[5:], case_count, str(zip))
-                        cur = conn.cursor()
-                        cur.execute('INSERT INTO covid_cases (date, total_cases, zipcode) VALUES(?, ?, ?)', params)
-
                     
-        print(f"{csv} done")
+                    try:
+                        date = f"total{m}_01_{year}"
+                        case_count = row[date]
 
-# when loading crime data, check if coor reverese geocoded is in zipcode table; if not, add to zipcode table
-# ^^^ to add any missing zipcodes missed from MD_Covid data
+                        if not math.isnan(case_count):
+                            params = (date[5:], case_count, str(zip))
+                            cur = conn.cursor()
+                            cur.execute('INSERT INTO covid_cases (date, total_cases, zipcode) VALUES(?, ?, ?)', params)
+                    except:
+                        pass
+                    
+        print(f"{csv} : done")
 
+# ----------------------data loaded----------------------
 '''
 table = pd.read_csv('../data/zipcodes_by_coor.csv')
 cur = conn.cursor()
