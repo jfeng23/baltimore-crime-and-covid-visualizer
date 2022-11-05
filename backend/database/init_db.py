05a257e00f6b69ea285446515b955f234e2684ce
jveres1 <98903692+jveres1@users.noreply.github.com>
2022-11-05 00:02:50 -0400

    Added crime data loading, renamed scripts folder

--- backend/database/init_db.py
+++ backend/database/init_db.py
@@ -2,6 +2,7 @@ import os
 import sqlite3
 import pandas as pd
 import math
+import time
 from geopy.geocoders import Nominatim
 
 if os.path.exists('baltimore_db.sqlite'):
@@ -16,11 +17,17 @@ conn.commit()
 
 print("Tables created successfully")
 
+zipcode_lookup = {} # Dictionary containing geocoded address/zipcode pairs
+
 def get_zipcode_from_address(address):
-    geolocator = Nominatim(user_agent="jfeng3@umbc.edu")
-    location = geolocator.geocode(address)
-    zipcode = location.address.split(",")[-2].strip()
-    return zipcode
+    global zipcode_lookup
+    # Check if address was already geocoded to avoid duplicate geocoding
+    if address not in zipcode_lookup:
+        geolocator = Nominatim(user_agent="jfeng3@umbc.edu")
+        location = geolocator.geocode(address)
+        zipcode = location.address.split(",")[-2].strip()
+        zipcode_lookup[address] = zipcode
+    return zipcode_lookup[address]
 
 
 # ----------------------load all csv files into db----------------------
@@ -66,7 +73,7 @@ for csv in os.listdir(PATH):
                 for i in range(1, 13):
                     if i < 10: m = f"0{i}"
                     else: m = i
-                    
+
                     try:
                         date = f"total{m}_01_{year}"
                         case_count = row[date]
@@ -78,12 +85,32 @@ for csv in os.listdir(PATH):
                     except:
                         pass
 
-        # load CRIME_part_1 into db
+    # load CRIME_part_1 into db
 
-        if "Part_1_Crime" in csv:
-            pass
-                    
-        print(f"{csv} : done")
+    if "Part_1_Crime" in csv:
+        table = pd.read_csv(PATH + csv, dtype='unicode')
+        # Load values from each row
+        for i, row in table.iterrows():
+            longitude = row.X
+            latitude = row.Y
+            date = row.CrimeDateTime
+            code = row.CrimeCode
+            type = row.Description
+            address = row.Location
+            # Skip the entry if there is no location information
+            if not address.strip():
+                continue
+            zipcode = get_zipcode_from_address(address)
+
+            # Add to database
+            params = (longitude, latitude, date, code, type, zipcode)
+            cur = conn.cursor()
+            cur.execute('INSERT INTO crime (long, lat, date, type_code, type, zipcode) VALUES(?, ?, ?, ?, ?, ?)', params)
+
+    print(f"{csv} : done")
+
+conn.commit()
+conn.close()
 
 # ----------------------data loaded----------------------
 '''
